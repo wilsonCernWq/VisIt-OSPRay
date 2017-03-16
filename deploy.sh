9e0e0e37b7601157a2d62f87ea9d81365b077eb2
wilson.over.cloud@gmail.com <wilson.over.cloud@gmail.com>
2017-03-16 15:24:01 -0600

    added vtkOSPRay inside

--- deploy.sh
+++ deploy.sh
@@ -2,22 +2,37 @@
 
 cd /home/sci/qwu/visitOSPRay/visitOSPRayCPU/pascal
 
-if [ "$1" == "source" ]; then 
+if [ "$1" == "plot" ]; then 
 
-    cp -r plots ../src-visit2.12.0/
+    cp -v plots/Volume/* ../src-visit2.12.0/plots/Volume    
 
-    cp -r avt/Filters/*.cpp ../src-visit2.12.0/avt/Filters
-    cp -r avt/Filters/*.C   ../src-visit2.12.0/avt/Filters
-    cp -r avt/Filters/*.h   ../src-visit2.12.0/avt/Filters
+elif [ "$1" == "filter" ]; then 
 
-    cp -r avt/Pipeline/Data/*.cpp ../src-visit2.12.0/avt/Pipeline/Data
-    cp -r avt/Pipeline/Data/*.C   ../src-visit2.12.0/avt/Pipeline/Data
-    cp -r avt/Pipeline/Data/*.h   ../src-visit2.12.0/avt/Pipeline/Data
+    cp -v avt/Filters/*.C   ../src-visit2.12.0/avt/Filters
+    cp -v avt/Filters/*.h   ../src-visit2.12.0/avt/Filters
+
+elif [ "$1" == "pipeline" ]; then 
+
+    cp -v avt/Pipeline/Data/*.C   ../src-visit2.12.0/avt/Pipeline/Data
+    cp -v avt/Pipeline/Data/*.h   ../src-visit2.12.0/avt/Pipeline/Data
+
+elif [ "$1" == "source" ]; then
+
+    cp -v plots/Volume/* ../src-visit2.12.0/plots/Volume
+    cp -v avt/Filters/*.C   ../src-visit2.12.0/avt/Filters
+    cp -v avt/Filters/*.h   ../src-visit2.12.0/avt/Filters
+    cp -v avt/Pipeline/Data/*.C   ../src-visit2.12.0/avt/Pipeline/Data
+    cp -v avt/Pipeline/Data/*.h   ../src-visit2.12.0/avt/Pipeline/Data
+
+elif [ "$1" == "vtkospray" ]; then
+
+    gvfs-trash ../src-visit2.12.0/avt/Plotter/OSPRay/vtkOSPRay/*
+    cp -vr avt/Plotter/OSPRay/vtkOSPRay/* ../src-visit2.12.0/avt/Plotter/OSPRay/vtkOSPRay
 
 elif [ "$1" == "cmake" ]; then 
     
-    cp ./*.txt           ../src-visit2.12.0/
-    cp avt/Filters/*.txt ../src-visit2.12.0/avt/Filters/
+    cp -v ./*.txt           ../src-visit2.12.0/
+    cp -v avt/Filters/*.txt ../src-visit2.12.0/avt/Filters/
 
 fi
 
