6ee54804095f018a29439a0244bb786c75f8026c
Qi Wu <qwu@hastur.sci.utah.edu>
2017-05-23 20:22:24 -0600

    update ospray version

--- config-site/hastur.sci.utah.edu.cmake
+++ config-site/hastur.sci.utah.edu.cmake
@@ -36,7 +36,7 @@ VISIT_OPTION_DEFAULT(VISIT_THREAD OFF TYPE BOOL)
 ## -- recommend to remove tbb libraries inside embree and ospray binary folder  
 ##                                                                              
 SET(OSPRAY_USE_EXTERNAL_EMBREE ON)                                              
-SET(ospray_DIR /home/sci/qwu/software/ospray-qwu-29.03.2017/lib64/cmake/ospray-1.3.0)
+SET(ospray_DIR /home/sci/qwu/software/ospray-qwu-23.05.2017/lib64/cmake/ospray-1.3.0)
 SET(embree_DIR /home/sci/qwu/software/embree-2.14.0.x86_64.linux/lib/cmake/embree-2.14.0)
 SET(TBB_ROOT /home/sci/qwu/software/tbb2017_20160916oss)                          
 SET(ISPC_EXECUTABLE /home/sci/qwu/software/ispc-v1.9.1-linux)                     
