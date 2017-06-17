ef7a025819c9ce0e0323db31a60e0b047985853d
Qi WU <qwu@kepler.sci.utah.edu>
2017-06-17 16:20:48 -0600

    cmake config on hastur for trunk version

--- src/config-site/hastur.sci.utah.edu.cmake
+++ src/config-site/hastur.sci.utah.edu.cmake
@@ -1,7 +1,7 @@
-#/home/sci/qwu/VisIt/visit/cmake/3.0.2/linux-x86_64_gcc-4.8/bin/cmake
+#/home/sci/qwu/VisIt/visit/cmake/3.8.1/linux-x86_64_gcc-5.4/bin/cmake
 ##
-## ./build_visit2_12_0 generated host.cmake
-## created: Sat May 13 20:27:17 MDT 2017
+## ./build_visit generated host.cmake
+## created: Sat Jun 17 16:11:15 MDT 2017
 ## system: Linux hastur.sci.utah.edu 3.10.0-327.36.1.el7.x86_64 #1 SMP Sun Sep 18 13:04:29 UTC 2016 x86_64 x86_64 x86_64 GNU/Linux
 ## by: qwu
 
@@ -9,14 +9,14 @@
 ## Setup VISITHOME & VISITARCH variables.
 ##
 SET(VISITHOME /home/sci/qwu/VisIt/visit)
-SET(VISITARCH linux-x86_64_gcc-4.8)
+SET(VISITARCH linux-x86_64_gcc-5.4)
 
 ## Compiler flags.
 ##
 VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc TYPE FILEPATH)
 VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++ TYPE FILEPATH)
-VISIT_OPTION_DEFAULT(VISIT_C_FLAGS " -m64 -fPIC -fvisibility=hidden -fopenmp" TYPE STRING)
-VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS " -m64 -fPIC -fvisibility=hidden -fopenmp" TYPE STRING)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS " -m64 -fPIC -fvisibility=hidden" TYPE STRING)
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS " -m64 -fPIC -fvisibility=hidden" TYPE STRING)
 
 ##
 ## Parallel Build Setup.
@@ -61,13 +61,6 @@ SET(PIDX_DIR /home/sci/qwu/VisIt/visitOSPRayCPU/PIDX/install-master)
 ##
 
 ##
-## Mesa
-##
-## disable mesa for non client-server mode
-##
-## VISIT_OPTION_DEFAULT(VISIT_MESA_DIR ${VISITHOME}/mesa/7.10.2/${VISITARCH})
-
-##
 ## Python
 ##
 VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.7.11/${VISITARCH})
@@ -90,7 +83,6 @@ VISIT_OPTION_DEFAULT(VISIT_QWT_DIR ${VISITHOME}/qwt/${QWT_VERSION}/${VISITARCH})
 ##
 SETUP_APP_VERSION(VTK 6.1.0)
 VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/${VTK_VERSION}/${VISITARCH})
-##
 
 ##
 ## BOOST
@@ -113,6 +105,7 @@ VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz
 ## Ice-T
 ##
 VISIT_OPTION_DEFAULT(VISIT_ICET_DIR ${VISITHOME}/icet/1.0.0/${VISITARCH})
+##
 
 ##
 ## NetCDF
@@ -130,5 +123,5 @@ VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP
 ## Uintah
 ##
 SETUP_APP_VERSION(UINTAH 1.6.0)
-VISIT_OPTION_DEFAULT(VISIT_UINTAH_DIR ${VISITHOME}/uintah/1.6.0/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_UINTAH_DIR ${VISITHOME}/uintah/${UINTAH_VERSION}/${VISITARCH})
 
