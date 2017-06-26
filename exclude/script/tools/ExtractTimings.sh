3c44e009e201506f60b711ac1593004143b21b12
Qi WU <qwu@kepler.sci.utah.edu>
2017-06-26 14:54:08 -0600

    update visit scripts for benchmark

--- exclude/script/tools/ExtractTimings.sh
+++ exclude/script/tools/ExtractTimings.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 echo "" > ./$1.txt
-grep -r "Commit OSPRay patch" ./$1/* >> ./$1.txt
-grep -r "Render OSPRay patch" ./$1/* >> ./$1.txt
-grep -r "OSPRayRendering" ./$1/* >> ./$1.txt
-grep -r "Sample point extraction" ./$1/* >> ./$1.txt
-grep -r "Compositing" ./$1/* >> ./$1.txt
-grep -r "Ray Tracing" ./$1/* >> ./$1.txt
-grep -r "avtRayTracer" ./$1/* >> ./$1.txt
+grep -r "Commit OSPRay patch" ./$1/*.timings >> ./$1.txt
+grep -r "Render OSPRay patch" ./$1/*.timings >> ./$1.txt
+grep -r "AllPatchRendering" ./$1/*.timings >> ./$1.txt
+grep -r "Sample point extraction" ./$1/*.timings >> ./$1.txt
+grep -r "Compositing" ./$1/*.timings >> ./$1.txt
+grep -r "Ray Tracing" ./$1/*.timings >> ./$1.txt
+grep -r "avtRayTracer" ./$1/*.timings >> ./$1.txt
