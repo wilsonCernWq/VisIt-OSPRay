60a7665288d32b461c6be9061d203a09c604ed8c
Qi WU <qiwu@login3.stampede2.tacc.utexas.edu>
2017-10-31 02:39:47 -0500

    ospray weak scaling script

--- exclude/script/stampede2/weak/gensubmit_ospray.sh
+++ exclude/script/stampede2/weak/gensubmit_ospray.sh
@@ -1,21 +1,33 @@
+#!/bin/bash
+
+NT=$1 # number of tasks
+NN=$2 # number of node
+NC=$3 # number of cores per task
+NV=$((NN/1))
+NAME=n${NN}p$((NT / NN))
+
+mkdir -p $NAME
+cd $NAME
+
+EXE=$WORK/software/VisIt/stampede2/visit-trunk/build/bin/visit
+SCP=$(pwd)/input-$NAME.py
+
+cat > input-$NAME.py <<EOF
 import os
 import math
 from subprocess import call
 
-#-----------------------------------------------------------------------------
 server_path = "./"
 client_path = "./"
 datainfo = {
     'HOSTNAME': "localhost",
-    'FULLPATH': "/scratch/04915/qiwu/runs/weak/data-2/GradientVar_0.idx",
+    'FULLPATH': "/scratch/04915/qiwu/runs/weak/data/volume-${NV}/GradientVar.idx",
     'TIMESTEP': 0,
     'VARIABLE': "var_0"
 }
-#-----------------------------------------------------------------------------
-# functions
+
 def makePlot(atts, useOSPRay = True, usePascal = True, useDefault = True):
     OpenDatabase(datainfo['HOSTNAME'] + ":" + datainfo['FULLPATH'])
-    SetTimeSliderState(datainfo['TIMESTEP'])
     AddPlot("Volume", datainfo['VARIABLE'])
     def drawPlots(VolumeAtts, VolumeType):
         # Splatting, Texture3D, RayCasting, RayCastingIntegration
@@ -77,3 +89,30 @@ VolumeAtts.rendererSamples = 3
 # open remote
 makePlot(VolumeAtts, True, False, False)
 exit()
+EOF
+
+cat > sample-$NAME.sh <<EOF
+#!/bin/bash
+#SBATCH -n ${NT}
+#SBATCH -N ${NN}
+#SBATCH --time=00:30:00 # walltime, abbreviated by -t
+#
+# load modules
+module load intel
+module load impi
+module load hdf5
+export LIBRARY_PATH=${LIBRARY_PATH}:/work/04915/qiwu/stampede2/software/VisIt/stampede2/visit/vtk/6.1.0/linux-x86_64_gcc-5.4/lib
+export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/work/04915/qiwu/stampede2/software/VisIt/stampede2/visit/vtk/6.1.0/linux-x86_64_gcc-5.4/lib
+export I_MPI_PIN_DOMAIN=omp
+export OMP_NUM_THREADS=$NC
+export KMP_AFFINITY=verbose,granularity=core,compact,1,0
+export OSPRAY_THREADS=$NC
+#
+# run job
+echo "job: <\$SLURM_JOBID> node = $NN processes-per-node = $((NT / NN))"
+$EXE -np $NT -nn $NN -l ibrun \
+    -withhold-timing-output -timing \
+    -nowin -cli -s $SCP 
+EOF
+sbatch -p normal -A TG-ASC170049 sample-$NAME.sh
+cd -
