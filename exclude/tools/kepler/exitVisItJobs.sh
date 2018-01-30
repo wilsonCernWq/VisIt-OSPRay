56a1c3a55a198fca428445ec745b2bf7a1dddab3
Qi WU <qwu@kepler.sci.utah.edu>
2018-01-29 17:39:25 -0700

    reorganize files

--- exclude/tools/kepler/exitVisItJobs.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-DEST=$1
-cd /home/sci/qwu/
-mv $(ls -1 /home/sci/qwu/slurm-*.out | head -n 1) $DEST
-mv /home/sci/qwu/*.engine_par.*.vlog $DEST
-mv /home/sci/qwu/*.engine_ser.*.vlog $DEST
-mv /home/sci/qwu/engine_par.*.timings $DEST
-cd /home/sci/qwu/
-source /home/sci/qwu/software/Kepler/VisIt/visit-depend.sh deactivate
