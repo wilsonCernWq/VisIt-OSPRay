04d7a670d844f1dd02a4afcfb4510f6dd2750d90
WU Qi <wilson.over.cloud@gmail.com>
2018-06-11 21:52:12 -0600

    temporary remove database since it still uses VTK6

---
+++ src/config-site/lagodelcielo.cmake
@@ -0,0 +1,136 @@
+#/ssd/users/qwu/VisIt/cmake/3.8.1/linux-x86_64_gcc-7.3/bin/cmake
+##
+## ./build_visit generated host.cmake
+## created: Mon Jun 11 02:30:35 MDT 2018
+## system: Linux lagodelcielo 4.4.120-45-default #1 SMP Wed Mar 14 20:51:49 UTC 2018 (623211f) x86_64 x86_64 x86_64 GNU/Linux
+## by: qwu
+
+##
+## Setup VISITHOME & VISITARCH variables.
+##
+SET(VISITHOME /ssd/users/qwu/VisIt)
+SET(VISITARCH linux-x86_64_gcc-7.3)
+
+## Compiler flags.
+##
+VISIT_OPTION_DEFAULT(VISIT_C_COMPILER gcc TYPE FILEPATH)
+VISIT_OPTION_DEFAULT(VISIT_CXX_COMPILER g++ TYPE FILEPATH)
+VISIT_OPTION_DEFAULT(VISIT_FORTRAN_COMPILER /usr/bin/gfortran TYPE FILEPATH)
+VISIT_OPTION_DEFAULT(VISIT_C_FLAGS " -m64 -fPIC  -O2 -g -fvisibility=hidden" TYPE STRING)
+VISIT_OPTION_DEFAULT(VISIT_CXX_FLAGS " -m64 -fPIC  -O2 -g -std=c++98 -fvisibility=hidden" TYPE STRING)
+
+##
+## Parallel Build Setup.
+##
+VISIT_OPTION_DEFAULT(VISIT_PARALLEL ON TYPE BOOL)
+## (configured w/ mpi compiler wrapper)
+VISIT_OPTION_DEFAULT(VISIT_MPI_COMPILER /opt/intel/compilers_and_libraries_2018.1.163/linux/mpi/intel64/bin/mpicc TYPE FILEPATH)
+
+##
+## VisIt Thread Option
+##
+VISIT_OPTION_DEFAULT(VISIT_THREAD OFF TYPE BOOL)
+
+##############################################################
+##
+## Database reader plugin support libraries
+##
+## The HDF4, HDF5 and NetCDF libraries must be first so that
+## their libdeps are defined for any plugins that need them.
+##
+## For libraries with LIBDEP settings, order matters.
+## Libraries with LIBDEP settings that depend on other
+## Library's LIBDEP settings must come after them.
+##############################################################
+##
+
+##
+## Python
+##
+VISIT_OPTION_DEFAULT(VISIT_PYTHON_DIR ${VISITHOME}/python/2.7.14/${VISITARCH})
+
+##
+## Qt
+##
+SETUP_APP_VERSION(QT 5.10.1)
+VISIT_OPTION_DEFAULT(VISIT_QT_DIR ${VISITHOME}/qt/${QT_VERSION}/${VISITARCH})
+
+##
+## QWT
+##
+SETUP_APP_VERSION(QWT 6.1.2)
+VISIT_OPTION_DEFAULT(VISIT_QWT_DIR ${VISITHOME}/qwt/${QWT_VERSION}/${VISITARCH})
+
+##
+## VTK
+##
+SETUP_APP_VERSION(VTK 8.1.0)
+VISIT_OPTION_DEFAULT(VISIT_VTK_DIR ${VISITHOME}/vtk/${VTK_VERSION}/${VISITARCH})
+
+##
+## SZIP
+##
+VISIT_OPTION_DEFAULT(VISIT_SZIP_DIR ${VISITHOME}/szip/2.1/${VISITARCH})
+
+##
+## HDF5
+##
+VISIT_OPTION_DEFAULT(VISIT_HDF5_DIR ${VISITHOME}/hdf5/1.8.14/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_HDF5_LIBDEP ${VISITHOME}/szip/2.1/${VISITARCH}/lib sz /usr/lib z TYPE STRING)
+
+##
+## BOOST
+##
+SETUP_APP_VERSION(BOOST 1_67_0)
+VISIT_OPTION_DEFAULT(VISIT_BOOST_DIR /usr)
+
+##
+## EMBREE
+##
+VISIT_OPTION_DEFAULT(VISIT_EMBREE_ROOT ${VISITHOME}/embree/${VISITARCH}/embree-2.16.5.x86_64.linux)
+
+##
+## Ice-T
+##
+VISIT_OPTION_DEFAULT(VISIT_ICET_DIR ${VISITHOME}/icet/77c708f9090236b576669b74c53e9f105eedbd7e/${VISITARCH})
+
+##
+## ISPC
+##
+VISIT_OPTION_DEFAULT(VISIT_ISPC_ROOT ${VISITHOME}/ispc/${VISITARCH}/ispc-v1.9.2-linux)
+##
+
+##
+## NetCDF
+##
+VISIT_OPTION_DEFAULT(VISIT_NETCDF_DIR ${VISITHOME}/netcdf/4.1.1/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_NETCDF_LIBDEP HDF5_LIBRARY_DIR hdf5_hl HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP} TYPE STRING)
+
+##
+## TBB
+##
+VISIT_OPTION_DEFAULT(TBB_ROOT ${VISITHOME}/tbb/${VISITARCH}/tbb2018_20171205oss)
+
+##
+## OSPRay
+##
+VISIT_OPTION_DEFAULT(VISIT_OSPRAY ON TYPE BOOL)
+VISIT_OPTION_DEFAULT(VISIT_OSPRAY_DIR ${VISITHOME}/ospray/1.4.3/${VISITARCH}/lib64/cmake/ospray-1.4.3)
+
+##
+## PIDX
+##
+SETUP_APP_VERSION(PIDX 0.9.1)
+VISIT_OPTION_DEFAULT(VISIT_PIDX_DIR /home/sci/qwu/software/Lagodelcielo/PIDX/install)
+
+##
+## Silo
+##
+VISIT_OPTION_DEFAULT(VISIT_SILO_DIR ${VISITHOME}/silo/4.10.2/${VISITARCH})
+VISIT_OPTION_DEFAULT(VISIT_SILO_LIBDEP HDF5_LIBRARY_DIR hdf5 ${VISIT_HDF5_LIBDEP} /usr/lib z TYPE STRING)
+
+##
+## Uintah
+##
+# SETUP_APP_VERSION(UINTAH 2.5.0)
+# VISIT_OPTION_DEFAULT(VISIT_UINTAH_DIR ${VISITHOME}/uintah/${UINTAH_VERSION}/${VISITARCH})
