e42b67b33d2a3a7ff67e42f6cfa1bd415a2a1b4a
WU Qi <wilson.over.cloud@gmail.com>
2018-06-20 19:17:20 -0600

    regenerate config

--- src/config-site/lagodelcielo.cmake
+++ src/config-site/lagodelcielo.cmake
@@ -1,7 +1,7 @@
 #/ssd/users/qwu/VisIt/3rdparty/trunk/cmake/3.8.1/linux-x86_64_gcc-7.3/bin/cmake
 ##
 ## ./build_visit generated host.cmake
-## created: Wed Jun 20 18:57:46 MDT 2018
+## created: Wed Jun 20 19:11:04 MDT 2018
 ## system: Linux lagodelcielo 4.4.120-45-default #1 SMP Wed Mar 14 20:51:49 UTC 2018 (623211f) x86_64 x86_64 x86_64 GNU/Linux
 ## by: qwu
 
