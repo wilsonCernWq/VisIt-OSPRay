2563b74f051c06180e10952fe027ff68959188fb
WU Qi <wilson.over.cloud@gmail.com>
2018-07-16 19:08:59 -0600

    updates

--- exclude/tools/build/premake_lagodelcielo.sh
+++ exclude/tools/build/premake_lagodelcielo.sh
@@ -37,12 +37,14 @@ ARGS=${ARGS}" --thirdparty-path $DIR_INSTALL "
 ARGS=${ARGS}" --installation-build-dir $DIR_BUILD "
 
 if   [[ "$PREMAKE_VERSION" == "trunk" ]]; then
-    ARGS=${ARGS}" --skip-opengl-context-check "
     ARGS=${ARGS}" --alt-ospray-dir /home/sci/qwu/OSPRay/Lagodelcielo/install-visit/lib64/cmake/ospray-1.6.0 "
     ARGS=${ARGS}" --pidx "
+    ARGS=${ARGS}" --skip-opengl-context-check "
+    ARGS=${ARGS}" --osmesa --llvm "
 elif [[ "$PREMAKE_VERSION" == "rc2.13" ]]; then
-    ARGS=${ARGS}" --slivr --uintah --ospray "
+    ARGS=${ARGS}" --ospray "
     ARGS=${ARGS}" --alt-pidx-dir /home/sci/qwu/software/Lagodelcielo/PIDX/install "
+    ARGS=${ARGS}" --slivr --uintah "
 fi
 
 # ------------------------------ 
