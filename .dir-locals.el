a1135df7e9ff200e2a9c23175c9a868f80dd479c
WU Qi <wilson.over.cloud@gmail.com>
2018-08-05 21:23:23 -0600

    remove .dir-local.el

--- .dir-locals.el
+++
@@ -1,7 +0,0 @@
-;;; Directory Local Variables
-;;; See Info node `(emacs) Directory Variables' for more information.
-
-((nil . ((indent-tabs-mode . nil)
-		 (tab-width . 4)))
- (c-mode . ((c-file-style . "linux")
-			(c-basic-offset . 4))))
